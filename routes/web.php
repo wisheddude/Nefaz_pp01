7c4a0e88b5a1b378652c25b76de988a0157e349d
CosmicRapture <wisheddude@gmail.com>
2026-03-05 02:58:37 +0500

    Обновление маршрутов

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [AuthController::class, 'login']);
